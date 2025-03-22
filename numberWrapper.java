381d13ada5fcaa210abb734c80e78bcc685e3795
toktobek <toktobek.d@gmail.com>
2025-03-22 02:20:18 -0500

    loops example

---
+++ numberWrapper.java
@@ -0,0 +1,25 @@
+public class numberWrapper {
+    public static void main(String[] args) {
+        int myAge = Integer.parseInt(args[0]);
+        System.out.printf("You will be %d  years in 15 years", myAge + 15);
+        //int num1 = 7;
+        //Integer numB = Integer.valueOf(num1);
+        Double myDouble = Double.valueOf(234.12);
+        Float myFloat = Float.valueOf(23.14f);
+        byte myByte = Byte.valueOf("23");
+
+        //storeData(num1);
+
+        //int age = Integer.parseInt("45");
+        //double amount = Double.parseDouble("345.423");
+       // boolean flag = Boolean.parseBoolean("true");
+        //System.out.println(flag);
+
+
+
+    }
+
+    //public static void storeData(Object object) {
+
+    }
+
